0f47b6d1c905a6b429a9abf6bd2cb415fe622032
Michael Clifford <mcliffor@redhat.com>
2022-10-26 14:18:23 -0400

    update appwapper generator (#13)

--- src/codeflare_sdk/cluster/config.py
+++ src/codeflare_sdk/cluster/config.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class ClusterConfiguration:
     name: str
-    head_info: list = []
-    machine_types: list = [] #["m4.xlarge", "g4dn.xlarge"]
+    head_info: list = field(default_factory=list)
+    machine_types: list = field(default_factory=list) #["m4.xlarge", "g4dn.xlarge"]
     min_cpus: int = 1
     max_cpus: int = 1
     min_worker: int = 1
@@ -14,5 +14,6 @@ class ClusterConfiguration:
     gpu: int = 0
     template: str = "src/codeflare_sdk/templates/new-template.yaml"
     instascale: bool = False
-    envs: dict = {}
+    envs: dict = field(default_factory=dict)
     image: str = "ghcr.io/ibm-ai-foundation/base:ray1.13.0-py38-gpu-pytorch1.12.0cu116-20220826-202124"
+
