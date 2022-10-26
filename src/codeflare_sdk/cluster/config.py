fc8b2b719006606aacb38d00061f91d6553e8ba0
maxusmusti <meyceoz@redhat.com>
2022-10-26 12:35:10 -0400

    Updated new general yaml generation

--- src/codeflare_sdk/cluster/config.py
+++ src/codeflare_sdk/cluster/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class ClusterConfiguration:
     name: str
     head_info: list = []
-    machine_types: list = []
+    machine_types: list = ["m4.xlarge", "g4dn.xlarge"]
     min_cpus: int = 1
     max_cpus: int = 1
     min_worker: int = 1
@@ -12,7 +12,7 @@ class ClusterConfiguration:
     min_memory: int = 2
     max_memory: int = 2
     gpu: int = 0
-    template: str = "src/codeflare_sdk/templates/base-template.yaml"
+    template: str = "src/codeflare_sdk/templates/new-template.yaml"
     instascale: bool = False
     envs: dict = {}
     image: str = "rayproject/ray:latest"
