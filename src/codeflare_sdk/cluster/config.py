52b94c405f5862bcf4ebc1406bc809f65c3d0555
Eoin Gallinagh <34043122+VanillaSpoon@users.noreply.github.com>
2023-06-29 12:01:04 +0100

    Add support for image pull secrets for Ray Cluster images (#162)
    * add: Support for image pull secrets for Ray Cluster images
    
    * test: adjust unit tests to incorporate image_pull_secrets
    
    * refactor: refactor update_image_pull_secrets

--- src/codeflare_sdk/cluster/config.py
+++ src/codeflare_sdk/cluster/config.py
@@ -49,3 +49,4 @@ class ClusterConfiguration:
     envs: dict = field(default_factory=dict)
     image: str = "ghcr.io/foundation-model-stack/base:ray2.1.0-py38-gpu-pytorch1.12.0cu116-20221213-193103"
     local_interactive: bool = False
+    image_pull_secrets: list = field(default_factory=list)
