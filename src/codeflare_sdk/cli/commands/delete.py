21ede6101e1bcf3905ba59d35231d8f90b29378e
carsonmh <thecarsonharrell@gmail.com>
2023-07-26 15:46:41 -0700

    add: load_auth in delete function

--- src/codeflare_sdk/cli/commands/delete.py
+++ src/codeflare_sdk/cli/commands/delete.py
@@ -1,6 +1,7 @@
 import click
 
 from codeflare_sdk.cluster.cluster import get_cluster
+from codeflare_sdk.cli.cli_utils import load_auth
 
 
 @click.group()
@@ -12,6 +13,7 @@ def cli():
 @click.option("--name", type=str)
 @click.option("--namespace", type=str, default="default")
 def raycluster(name, namespace):
+    load_auth()
     cluster = get_cluster(name, namespace)
     cluster.down()
     click.echo(f"Cluster deleted successfully")
