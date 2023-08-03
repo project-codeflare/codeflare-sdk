2e4fdca3aeb03922e55f9f2f50474163957d23a1
carsonmh <thecarsonharrell@gmail.com>
2023-08-03 14:33:20 -0700

    create: list jobs CLI command

--- src/codeflare_sdk/cli/commands/list.py
+++ src/codeflare_sdk/cli/commands/list.py
@@ -1,11 +1,15 @@
 import click
-from kubernetes import client, config
 
 from codeflare_sdk.cluster.cluster import (
     list_clusters_all_namespaces,
     list_all_clusters,
 )
 from codeflare_sdk.cli.cli_utils import PluralAlias
+from codeflare_sdk.cluster.cluster import get_cluster
+from codeflare_sdk.cluster.cluster import _copy_to_ray
+from codeflare_sdk.cli.cli_utils import list_all_jobs
+from codeflare_sdk.cli.cli_utils import list_all_kubernetes_jobs
+from codeflare_sdk.cli.cli_utils import list_raycluster_jobs
 
 
 @click.group(cls=PluralAlias)
@@ -28,3 +32,19 @@ def raycluster(ctx, namespace, all):
         list_all_clusters(namespace)
         return
     list_clusters_all_namespaces()
+
+
+@cli.command()
+@click.option("--cluster-name", "-c", type=str)
+@click.option("--namespace", type=str)
+@click.option("--all", is_flag=True)
+def job(cluster_name, namespace, all):
+    """List all jobs in a specified RayCluster or in K8S cluster"""
+    if all:
+        list_all_jobs(True)
+        return
+    if cluster_name:
+        cluster = get_cluster(cluster_name, namespace)
+        list_raycluster_jobs(_copy_to_ray(cluster), True)
+        return
+    list_all_kubernetes_jobs(True)
