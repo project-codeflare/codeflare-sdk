d545c038f22104b8aa0f9832ce271c6297b1060f
carsonmh <thecarsonharrell@gmail.com>
2023-08-03 11:34:29 -0700

    create: submit job command cli

--- src/codeflare_sdk/cli/commands/submit.py
+++ src/codeflare_sdk/cli/commands/submit.py
@@ -1,6 +1,10 @@
 import click
 
 from codeflare_sdk.cluster.cluster import Cluster
+import pickle
+from torchx.runner import get_runner
+
+from codeflare_sdk.cluster.cluster import get_cluster
 
 
 @click.group()
@@ -30,3 +34,36 @@ def raycluster(name, wait):
         return
     cluster.up()
     cluster.wait_ready()
+
+
+@cli.command()
+@click.pass_context
+@click.argument("name", type=str)
+@click.option("--cluster-name", type=str)
+@click.option("--namespace", type=str, required=True)
+def job(ctx, name, cluster_name, namespace):
+    """
+    Submit a defined job to the Kubernetes cluster or a RayCluster
+    """
+    runner = get_runner()
+    try:
+        job_path = ctx.obj.codeflare_path + f"/{name}"
+        with open(job_path, "rb") as file:
+            job_def = pickle.load(file)
+    except Exception as e:
+        click.echo(
+            f"Error submitting job. Make sure the job is defined before submitting it"
+        )
+        return
+    if not cluster_name:
+        job = job_def.submit()
+        submission_id = runner.describe(job._app_handle).name.split(":")[1]
+        click.echo(f"{submission_id} submitted successfully")
+        return
+    cluster = get_cluster(cluster_name, namespace)
+    job = job_def.submit(cluster)
+    full_name = runner.describe(job._app_handle).name
+    submission_id = full_name[full_name.rfind(name) :]
+    click.echo(
+        f"{submission_id} submitted onto {cluster_name} RayCluster successfully\nView dashboard: {cluster.cluster_dashboard_uri()}"
+    )
