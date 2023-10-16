cc1dcd7860b789ffda8575a392cee447b4817686
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2023-10-16 16:09:14 +0100

    Update RayCluster labels to workload.codeflare.dev/appwrappers

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -743,7 +743,7 @@ def get_ray_obj(group, version, namespace, plural, cls=None):
                     "creationTimestamp": "2023-02-22T16:26:07Z",
                     "generation": 1,
                     "labels": {
-                        "appwrapper.mcad.ibm.com": "quicktest",
+                        "workload.codeflare.dev/appwrapper": "quicktest",
                         "controller-tools.k8s.io": "1.0",
                         "resourceName": "quicktest",
                         "orderedinstance": "m4.xlarge_g4dn.xlarge",
@@ -756,7 +756,7 @@ def get_ray_obj(group, version, namespace, plural, cls=None):
                                 "f:metadata": {
                                     "f:labels": {
                                         ".": {},
-                                        "f:appwrapper.mcad.ibm.com": {},
+                                        "f:workload.codeflare.dev/appwrapper": {},
                                         "f:controller-tools.k8s.io": {},
                                         "f:resourceName": {},
                                     },
@@ -1114,7 +1114,7 @@ def get_aw_obj(group, version, namespace, plural):
                                     "kind": "RayCluster",
                                     "metadata": {
                                         "labels": {
-                                            "appwrapper.mcad.ibm.com": "quicktest1",
+                                            "workload.codeflare.dev/appwrapper": "quicktest1",
                                             "controller-tools.k8s.io": "1.0",
                                         },
                                         "name": "quicktest1",
@@ -1437,7 +1437,7 @@ def get_aw_obj(group, version, namespace, plural):
                                     "kind": "RayCluster",
                                     "metadata": {
                                         "labels": {
-                                            "appwrapper.mcad.ibm.com": "quicktest2",
+                                            "workload.codeflare.dev/appwrapper": "quicktest2",
                                             "controller-tools.k8s.io": "1.0",
                                         },
                                         "name": "quicktest2",
