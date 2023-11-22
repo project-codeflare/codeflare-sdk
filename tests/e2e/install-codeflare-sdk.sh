2738b18a2d85d21cb787c7bfab0b87b43cad3f8c
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2023-11-22 19:57:54 +0000

    Reflect Poetry changes

--- tests/e2e/install-codeflare-sdk.sh
+++ tests/e2e/install-codeflare-sdk.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+# Generate zip file beforehand
+# tar -czvf root-dir.tar.gz --exclude='.git' --exclude='.github' --exclude='.pytest_cache' ./
+
+# Create temp-pod to contain the zip file
+# kubectl apply -f temp-pod.yaml
+
+# Copy the zip file to the temp-pod:/mnt
+# kubectl cp root-dir.tar.gz temp-pod:/mnt
+
+# Run a shell in the temp-pod
+# kubectl exec -it temp-pod -- /bin/sh
+
+# Unzip the zip file
+# tar -xzvf /mnt/root-dir.tar.gz -C /mnt
+
+# Not necessary as the PVC is mounted to /codeflare-sdk
+# mv /mnt/* /codeflare-sdk/
+
+## Copy files from temp-pod /mnt into the codeflare-sdk volume mount in this other pod.
+# kubectl cp default/temp-pod:/mnt codeflare-sdk
+
+
+cd ..
+
 # Install Poetry and configure virtualenvs
 pip install poetry
 poetry config virtualenvs.create false
 
-# Clone the CodeFlare SDK repository
-echo "first ls"
-ls
-pip uninstall codeflare-sdk
-echo "install codeflare sdk"
-pip install codeflare_sdk-0.0.0.dev0-py3-none-any.whl
-echo "second ls"
-ls
-# git clone --branch main https://github.com/project-codeflare/codeflare-sdk.git
-# cd codeflare-sdk
+cd codeflare-sdk
 
-# # Lock dependencies and install them
-# poetry lock --no-update
-# poetry install --with test,docs
+# Lock dependencies and install them
+poetry lock --no-update
+poetry install --with test,docs
 
-# Return to the previous directory
-# cd ..
+# Return to the workdir
+cd ..
+cd workdir
