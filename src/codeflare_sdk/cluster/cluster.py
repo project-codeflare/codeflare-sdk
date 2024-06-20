8fafd1a67fdd6e5b16aaac324408e850cb8bd69e
Bobbins228 <markcampbell114@gmail.com>
2024-06-20 12:28:35 +0100

    Revert "Added custom Volumes and Volume Mounts support"
    This reverts commit c68d425377105a30ec591364f73b993991afaa47.

--- src/codeflare_sdk/cluster/cluster.py
+++ src/codeflare_sdk/cluster/cluster.py
@@ -152,8 +152,6 @@ class Cluster:
         write_to_file = self.config.write_to_file
         local_queue = self.config.local_queue
         labels = self.config.labels
-        volumes = self.config.volumes
-        volume_mounts = self.config.volume_mounts
         return generate_appwrapper(
             name=name,
             namespace=namespace,
@@ -174,8 +172,6 @@ class Cluster:
             write_to_file=write_to_file,
             local_queue=local_queue,
             labels=labels,
-            volumes=volumes,
-            volume_mounts=volume_mounts,
         )
 
     # creates a new cluster with the provided or default spec
