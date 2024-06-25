53bf080836d1606ad19176e71e2b1321cb1da982
Suyash Nalawade <sunalawa@redhat.com>
2024-06-25 22:02:32 +0530

    Added support for checking version of the codeflare-sdk package   - Resolves: Issue #504 [https://github.com/project-codeflare/codeflare-sdk/issues/504]

--- src/codeflare_sdk/__init__.py
+++ src/codeflare_sdk/__init__.py
@@ -19,3 +19,11 @@ from .cluster import (
 from .job import RayJobClient
 
 from .utils import generate_cert
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("codeflare-sdk") #Update with latest version with each release
+
+except PackageNotFoundError:
+    __version__ = "unknown"
