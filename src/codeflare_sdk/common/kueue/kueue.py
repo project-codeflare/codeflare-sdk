b7c37af4e59cd5ac686b3d37f2424455b8ae9b62
Kevin <kpostlet@redhat.com>
2024-10-09 15:49:54 -0400

    add function for listing lqs by flavors
    Signed-off-by: Kevin <kpostlet@redhat.com>

--- src/codeflare_sdk/common/kueue/kueue.py
+++ src/codeflare_sdk/common/kueue/kueue.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
+from typing import Optional, List
 from codeflare_sdk.common import _kube_api_error_handling
 from codeflare_sdk.common.kubernetes_cluster.auth import config_check, get_api_client
 from kubernetes import client
@@ -43,6 +43,53 @@ def get_default_kueue_name(namespace: str):
             == "true"
         ):
             return lq["metadata"]["name"]
+
+
+def list_local_queues(
+    namespace: Optional[str] = None, flavors: Optional[List[str]] = None
+) -> List[dict]:
+    """
+    This function lists all local queues in the namespace provided.
+
+    If no namespace is provided, it will use the current namespace. If flavors is provided, it will only return local
+    queues that support all the flavors provided.
+
+    Note:
+        Depending on the version of the local queue API, the available flavors may not be present in the response.
+
+    Args:
+        namespace (str, optional): The namespace to list local queues from. Defaults to None.
+        flavors (List[str], optional): The flavors to filter local queues by. Defaults to None.
+    Returns:
+        List[dict]: A list of dictionaries containing the name of the local queue and the available flavors
+    """
+
+    from ...ray.cluster.cluster import get_current_namespace
+
+    if namespace is None:  # pragma: no cover
+        namespace = get_current_namespace()
+    try:
+        config_check()
+        api_instance = client.CustomObjectsApi(get_api_client())
+        local_queues = api_instance.list_namespaced_custom_object(
+            group="kueue.x-k8s.io",
+            version="v1beta1",
+            namespace=namespace,
+            plural="localqueues",
+        )
+    except ApiException as e:  # pragma: no cover
+        return _kube_api_error_handling(e)
+    to_return = []
+    for lq in local_queues["items"]:
+        item = {"name": lq["metadata"]["name"]}
+        if "flavors" in lq["status"]:
+            item["flavors"] = [f["name"] for f in lq["status"]["flavors"]]
+            if flavors is not None and not set(flavors).issubset(set(item["flavors"])):
+                continue
+        elif flavors is not None:
+            continue  # NOTE: may be indicative old local queue API and might be worth while raising or warning here
+        to_return.append(item)
+    return to_return
 
 
 def local_queue_exists(namespace: str, local_queue_name: str):
