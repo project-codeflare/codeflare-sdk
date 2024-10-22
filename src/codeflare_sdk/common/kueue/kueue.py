c2761cd48492fa9a5c14cff2264287ddd89b8c05
Ignas Baranauskas <ibaranau@redhat.com>
2024-10-22 12:37:01 +0100

    docs: enhance common module code documentation

--- src/codeflare_sdk/common/kueue/kueue.py
+++ src/codeflare_sdk/common/kueue/kueue.py
@@ -19,8 +19,23 @@ from kubernetes import client
 from kubernetes.client.exceptions import ApiException
 
 
-def get_default_kueue_name(namespace: str):
-    # If the local queue is set, use it. Otherwise, try to use the default queue.
+def get_default_kueue_name(namespace: str) -> Optional[str]:
+    """
+    Retrieves the default Kueue name from the provided namespace.
+
+    This function attempts to fetch the local queues in the given namespace and checks if any of them is annotated
+    as the default queue. If found, the name of the default queue is returned.
+
+    The default queue is marked with the annotation "kueue.x-k8s.io/default-queue" set to "true."
+
+    Args:
+        namespace (str):
+            The Kubernetes namespace where the local queues are located.
+
+    Returns:
+        Optional[str]:
+            The name of the default queue if it exists, otherwise None.
+    """
     try:
         config_check()
         api_instance = client.CustomObjectsApi(get_api_client())
@@ -58,12 +73,14 @@ def list_local_queues(
         Depending on the version of the local queue API, the available flavors may not be present in the response.
 
     Args:
-        namespace (str, optional): The namespace to list local queues from. Defaults to None.
-        flavors (List[str], optional): The flavors to filter local queues by. Defaults to None.
+        namespace (str, optional):
+            The namespace to list local queues from. Defaults to None.
+        flavors (List[str], optional):
+            The flavors to filter local queues by. Defaults to None.
     Returns:
-        List[dict]: A list of dictionaries containing the name of the local queue and the available flavors
+        List[dict]:
+            A list of dictionaries containing the name of the local queue and the available flavors
     """
-
     from ...ray.cluster.cluster import get_current_namespace
 
     if namespace is None:  # pragma: no cover
@@ -92,8 +109,22 @@ def list_local_queues(
     return to_return
 
 
-def local_queue_exists(namespace: str, local_queue_name: str):
-    # get all local queues in the namespace
+def local_queue_exists(namespace: str, local_queue_name: str) -> bool:
+    """
+    Checks if a local queue with the provided name exists in the given namespace.
+
+    This function queries the local queues in the specified namespace and verifies if any queue matches the given name.
+
+    Args:
+        namespace (str):
+            The namespace where the local queues are located.
+        local_queue_name (str):
+            The name of the local queue to check for existence.
+
+    Returns:
+        bool:
+            True if the local queue exists, False otherwise.
+    """
     try:
         config_check()
         api_instance = client.CustomObjectsApi(get_api_client())
@@ -113,6 +144,24 @@ def local_queue_exists(namespace: str, local_queue_name: str):
 
 
 def add_queue_label(item: dict, namespace: str, local_queue: Optional[str]):
+    """
+    Adds a local queue name label to the provided item.
+
+    If the local queue is not provided, the default local queue for the namespace is used. The function validates if the
+    local queue exists, and if it does, the local queue name label is added to the resource metadata.
+
+    Args:
+        item (dict):
+            The resource where the label will be added.
+        namespace (str):
+            The namespace of the local queue.
+        local_queue (str, optional):
+            The name of the local queue to use. Defaults to None.
+
+    Raises:
+        ValueError:
+            If the provided or default local queue does not exist in the namespace.
+    """
     lq_name = local_queue or get_default_kueue_name(namespace)
     if lq_name == None:
         return
