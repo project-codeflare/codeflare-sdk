0397b18737584bdf5be2bce3001974a075eed4fd
Bryan Keane <bkeane@redhat.com>
2025-12-04 18:46:23 +0000

    Update Ray to v2.52.1 for RHOAI 2.25.1 (#954)
    * NO-JIRA: Update Ray to 2.52.1
    
    * [2.25] fix: upgrade test post CVE fixes
    
    * fix: post ray 2.52.1 fix for mnist script
    
    * Update python 3.11 image sha
    
    * Update python 3.12 image sha
    
    * Add v0.31 to the CI branches
    
    ---------
    
    Co-authored-by: Pawel Paszki <ppaszki@redhat.com>

--- src/codeflare_sdk/common/utils/constants.py
+++ src/codeflare_sdk/common/utils/constants.py
@@ -1,11 +1,11 @@
-RAY_VERSION = "2.47.1"
+RAY_VERSION = "2.52.1"
 """
 The below are used to define the default runtime image for the Ray Cluster.
-* For python 3.11:ray:2.47.1-py311-cu121
-* For python 3.12:ray:2.47.1-py312-cu128
+* For python 3.11:ray:2.52.1-py311-cu121
+* For python 3.12:ray:2.52.1-py312-cu128
 """
-CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:6d076aeb38ab3c34a6a2ef0f58dc667089aa15826fa08a73273c629333e12f1e"
-CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:fb6f207de63e442c67bb48955cf0584f3704281faf17b90419cfa274fdec63c5"
+CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:595b3acd10244e33fca1ed5469dccb08df66f470df55ae196f80e56edf35ad5a"
+CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:6b135421b6e756593a58b4df6664f82fc4b55237ca81475f2867518f15fe6d84"
 
 # Centralized image selection
 SUPPORTED_PYTHON_VERSIONS = {
