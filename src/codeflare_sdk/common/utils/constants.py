da9bf4921019e68180ed5706af1430cc02f69de9
kryanbeane <bryankeane0@gmail.com>
2025-12-04 14:08:17 +0000

    Update runtime image shas

--- src/codeflare_sdk/common/utils/constants.py
+++ src/codeflare_sdk/common/utils/constants.py
@@ -4,8 +4,8 @@ The below are used to define the default runtime image for the Ray Cluster.
 * For python 3.11:ray:2.52.1-py311-cu121
 * For python 3.12:ray:2.52.1-py312-cu128
 """
-CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:TODO"
-CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:TODO"
+CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:595b3acd10244e33fca1ed5469dccb08df66f470df55ae196f80e56edf35ad5a"
+CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:6b135421b6e756593a58b4df6664f82fc4b55237ca81475f2867518f15fe6d84"
 
 # Centralized image selection
 SUPPORTED_PYTHON_VERSIONS = {
