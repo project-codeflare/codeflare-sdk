258cc0bf1cf75c2e68c649a80bc875beb2357401
Pat O'Connor <paoconno@redhat.com>
2026-03-31 13:26:48 +0100

    task(RHOAIENG-51570): Update runtime sha for Ray 2.53.0
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- src/codeflare_sdk/common/utils/constants.py
+++ src/codeflare_sdk/common/utils/constants.py
@@ -5,7 +5,7 @@ The below are used to define the default runtime image for the Ray Cluster.
 * For python 3.12:ray:2.52.1-py312-cu128
 """
 CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:595b3acd10244e33fca1ed5469dccb08df66f470df55ae196f80e56edf35ad5a"
-CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:6b135421b6e756593a58b4df6664f82fc4b55237ca81475f2867518f15fe6d84"
+CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:7b9c6d524b64a07746caa7dc89e691fc40eb4c2b4e41ffde8361bcd8d3c94d68"
 
 # Centralized image selection
 SUPPORTED_PYTHON_VERSIONS = {
