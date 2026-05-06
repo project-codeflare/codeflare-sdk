37adcff391a84aef0d25587231128f40d6182e1b
GitHub Action <action@github.com>
2026-05-06 11:01:31 +0000

    Update version configuration - Ray version: 2.52.1 -> 2.54.1 - SDK version: 0.36.0 -> 0.37.0 - Updated py312 CUDA runtime image SHA - py312 CUDA tag -> 2.54.1-py312-cu128 - py312 ROCm tag -> 2.54.1-py312-rocm64

--- src/codeflare_sdk/common/utils/constants.py
+++ src/codeflare_sdk/common/utils/constants.py
@@ -1,11 +1,11 @@
-RAY_VERSION = "2.52.1"
+RAY_VERSION = "2.54.1"
 """
 The below are used to define the default runtime image for the Ray Cluster.
 * For python 3.11:ray:2.52.1-py311-cu121
-* For python 3.12:ray:2.52.1-py312-cu128
+* For python 3.12:ray:2.54.1-py312-cu128
 """
 CUDA_PY311_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:595b3acd10244e33fca1ed5469dccb08df66f470df55ae196f80e56edf35ad5a"
-CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:7b9c6d524b64a07746caa7dc89e691fc40eb4c2b4e41ffde8361bcd8d3c94d68"
+CUDA_PY312_RUNTIME_IMAGE = "quay.io/modh/ray@sha256:42fbc5d898cb9c7d202ee89308ef328838d42985ec384f2476d8f3356acd01cb"
 
 # Centralized image selection
 SUPPORTED_PYTHON_VERSIONS = {
